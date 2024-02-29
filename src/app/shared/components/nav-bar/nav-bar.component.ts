bd694975614e074faf8af6da6716da923bf87f73
nickTparedes <nick.paredes@tivit.com>
2024-02-28 19:59:41 -0500

    Modificando footer y navbar

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LocalStorageService } from '../../services/local-storage.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
+    
+  constructor(private localStorage: LocalStorageService) {}
+
+
 
 }
---
+++ src/app/shared/services/local-storage.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  constructor() { }
+
+  getLocalStorage(key: string) {
+    return localStorage.getItem(key);
+  }
+
+  setLocalStorage(key: string, value: string){
+    localStorage.setItem(key, value.toString());
+  }
+}
