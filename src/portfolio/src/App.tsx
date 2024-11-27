a2212f9f045b15fdea00a2c27f150756868f3c4d
nickparedes <nick.eugenio.paredes@gmail.com>
2024-11-26 22:50:48 -0500

    Added init project in vite

---
+++ src/portfolio/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>Navbar</p>
+      <p>content</p>
+      <p>footer</p>
+    </>
+  )
+}
+
+export default App
